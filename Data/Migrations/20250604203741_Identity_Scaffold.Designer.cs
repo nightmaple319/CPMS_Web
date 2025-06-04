3bfad8b5df0bd22eba04184297295fd139e70b15
Yi-Ming, Chen <nightmaple319@gmail.com>
2025-06-04 15:51:25 -0500

    Add Identity Pages

--- Data/Migrations/20250604203741_Identity_Scaffold.Designer.cs
+++ Data/Migrations/20250604203741_Identity_Scaffold.Designer.cs
@@ -4,6 +4,7 @@ using CPMS_Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CPMS_Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250604203741_Identity_Scaffold")]
+    partial class Identity_Scaffold
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20250604203741_Identity_Scaffold.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CPMS_Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Identity_Scaffold : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MaterialRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestNo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    RequestDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequesterId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ApproverId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ApprovalDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IssuerId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RejectReason = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Remarks = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaterialRequests_AspNetUsers_ApproverId",
+                        column: x => x.ApproverId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MaterialRequests_AspNetUsers_IssuerId",
+                        column: x => x.IssuerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MaterialRequests_AspNetUsers_RequesterId",
+                        column: x => x.RequesterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpareParts",
+                columns: table => new
+                {
+                    No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    plant_id = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    position_id = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    subposition_id = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    category = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    specification = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    last_updated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    remarks = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpareParts", x => x.No);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockCounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountNo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CountDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CounterId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockCounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockCounts_AspNetUsers_CounterId",
+                        column: x => x.CounterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Team = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    IsSuperUser = table.Column<bool>(type: "bit", nullable: false),
+                    IsManager = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfiles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SparePartNo = table.Column<int>(type: "int", nullable: false),
+                    TransactionType = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    PreviousQuantity = table.Column<int>(type: "int", nullable: false),
+                    NewQuantity = table.Column<int>(type: "int", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ReferenceNo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InventoryTransactions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_InventoryTransactions_SpareParts_SparePartNo",
+                        column: x => x.SparePartNo,
+                        principalTable: "SpareParts",
+                        principalColumn: "No",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaterialRequestDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<int>(type: "int", nullable: false),
+                    SparePartNo = table.Column<int>(type: "int", nullable: false),
+                    RequestedQuantity = table.Column<int>(type: "int", nullable: false),
+                    ApprovedQuantity = table.Column<int>(type: "int", nullable: false),
+                    IssuedQuantity = table.Column<int>(type: "int", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    MaterialRequestId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialRequestDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaterialRequestDetails_MaterialRequests_MaterialRequestId",
+                        column: x => x.MaterialRequestId,
+                        principalTable: "MaterialRequests",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_MaterialRequestDetails_MaterialRequests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "MaterialRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MaterialRequestDetails_SpareParts_SparePartNo",
+                        column: x => x.SparePartNo,
+                        principalTable: "SpareParts",
+                        principalColumn: "No",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockCountDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountId = table.Column<int>(type: "int", nullable: false),
+                    SparePartNo = table.Column<int>(type: "int", nullable: false),
+                    SystemQuantity = table.Column<int>(type: "int", nullable: false),
+                    CountedQuantity = table.Column<int>(type: "int", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockCountDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockCountDetails_SpareParts_SparePartNo",
+                        column: x => x.SparePartNo,
+                        principalTable: "SpareParts",
+                        principalColumn: "No",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StockCountDetails_StockCounts_CountId",
+                        column: x => x.CountId,
+                        principalTable: "StockCounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTransactions_SparePartNo",
+                table: "InventoryTransactions",
+                column: "SparePartNo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTransactions_UserId",
+                table: "InventoryTransactions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequestDetails_MaterialRequestId",
+                table: "MaterialRequestDetails",
+                column: "MaterialRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequestDetails_RequestId",
+                table: "MaterialRequestDetails",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequestDetails_SparePartNo",
+                table: "MaterialRequestDetails",
+                column: "SparePartNo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequests_ApproverId",
+                table: "MaterialRequests",
+                column: "ApproverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequests_IssuerId",
+                table: "MaterialRequests",
+                column: "IssuerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialRequests_RequesterId",
+                table: "MaterialRequests",
+                column: "RequesterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockCountDetails_CountId",
+                table: "StockCountDetails",
+                column: "CountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockCountDetails_SparePartNo",
+                table: "StockCountDetails",
+                column: "SparePartNo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockCounts_CounterId",
+                table: "StockCounts",
+                column: "CounterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryTransactions");
+
+            migrationBuilder.DropTable(
+                name: "MaterialRequestDetails");
+
+            migrationBuilder.DropTable(
+                name: "StockCountDetails");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "MaterialRequests");
+
+            migrationBuilder.DropTable(
+                name: "SpareParts");
+
+            migrationBuilder.DropTable(
+                name: "StockCounts");
+        }
+    }
+}
